cb2c4642c7cd4fee3981bba1974a6f1e4ed1d6f0
Kauan Soares Mota <4kauanmota@gmail.com>
2024-03-25 18:02:59 -0300

    :tada: First commit

---
+++ src/components/organisms/Calculator.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { computed, ref } from 'vue';
+
+  const expression = ref("");
+
+  const expressionConverted = computed(() => {
+    try{
+      return eval(expression.value);
+    }
+    catch {
+      return ""
+    }
+  })
+</script>
+
+<template>
+  <div id="container">
+    <span>{{ expressionConverted }}</span>
+  
+    <input type="text" v-model="expression" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  #container {
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+  }
+</style>
