51a49ef3ca72e27d2036db6254705ff3f6339423
Kauan Soares Mota <4kauanmota@gmail.com>
2024-03-25 22:52:12 -0300

    :sparkles: feat: Expression preview

--- src/components/organisms/Calculator.vue
+++ src/components/organisms/Calculator.vue
@@ -1,30 +1,79 @@
 <script setup lang="ts">
-  import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 
-  const expression = ref("");
+const expression = ref("");
 
-  const expressionConverted = computed(() => {
-    try{
-      return eval(expression.value);
-    }
-    catch {
-      return ""
-    }
-  })
+const expressionFormatted = computed(() => {
+  try {
+    let format: string = expression.value;
+
+    format = format.replace(/\*\*/g, "^");
+    format = format.replace(
+      /\^(\d+)(?=[^\d]|$)/g,
+      (_: string, exponent: string) => {
+        console.log(exponent);
+        return `<sup>${exponent}</sup>`;
+      }
+    );
+
+    format = format.replace(/\/(\d)/g, (_: string, exponent: string) => {
+      return `<hr>${exponent}`;
+    });
+
+    format = format.replace(/\+/g, " + ");
+    format = format.replace(/\-/g, "  -  ");
+    format = format.replace(/\*/g, " x ");
+    format = format.replace(/\%/g, " % ");
+
+    return format;
+  } catch {
+    return "pedro";
+  }
+});
+
+const expressionCalc = computed(() => {
+  try {
+    return eval(expression.value);
+  } catch {
+    return null;
+  }
+});
 </script>
 
 <template>
   <div id="container">
-    <span>{{ expressionConverted }}</span>
-  
+    <div class="expression">
+      <span v-html="expressionFormatted"></span>
+
+      <span class="result" v-if="expressionCalc || expressionCalc == 0">
+        <span> = </span>
+        <span>{{ expressionCalc }}</span>
+      </span>
+    </div>
+
     <input type="text" v-model="expression" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-  #container {
+#container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  .expression {
     display: flex;
-    flex-direction: column;
-    gap: 4rem;
+    align-items: center;
+    gap: 2rem;
+
+    & > span {
+      text-align: center;
+
+      &.result {
+        display: flex;
+        gap: 2rem;
+      }
+    }
   }
+}
 </style>
